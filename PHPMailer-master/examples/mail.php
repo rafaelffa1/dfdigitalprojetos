73292f3cdf4887278d6902626065126ac4b19fc0
rafaelffa1 <rafael.ferreira.felix.almeida@gmail.com>
2017-05-08 14:58:54 -0300

    a

--- PHPMailer-master/examples/mail.php
+++ PHPMailer-master/examples/mail.php
@@ -8,20 +8,20 @@ require 'PHPMailer-master/PHPMailerAutoload.php';
 //Create a new PHPMailer instance
 $mail = new PHPMailer;
 //Set who the message is to be sent from
-$mail->setFrom('rafael@azzurradigital.com', 'First Last');
+$mail->setFrom('rafael.ferreira.felix.almeida@gmail.com', 'First Last');
 //Set an alternative reply-to address
-$mail->addReplyTo('rafael@azzurradigital.com', 'First Last');
+$mail->addReplyTo('rafael.ferreira.felix.almeida@gmail.com', 'First Last');
 //Set who the message is to be sent to
-$mail->addAddress('rafael@azzurradigital.com', 'John Doe');
+$mail->addAddress('rafael.ferreira.felix.almeida@gmail.com', 'John Doe');
 //Set the subject line
 $mail->Subject = 'PHPMailer mail() test';
 //Read an HTML message body from an external file, convert referenced images to embedded,
 //convert HTML into a basic plain-text alternative body
-$mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
+$mail->msgHTML('<h1>Teste</h1>');
 //Replace the plain text body with one created manually
 $mail->AltBody = 'This is a plain-text message body';
 //Attach an image file
-$mail->addAttachment('PHPMailer-master/examples/images/phpmailer_mini.png');
+//$mail->addAttachment('PHPMailer-master/examples/images/phpmailer_mini.png');
 
 //send the message, check for errors
 if (!$mail->send()) {
